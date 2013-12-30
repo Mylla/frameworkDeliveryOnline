14b1aaaa26d86044bb9339c747ddae2381583262
Patricia <pneris13@gmail.com>
2013-12-30 01:12:36 -0300

    incluindo a classe loja

---
+++ FramDeliveryOnline/src/model/Loja.java
@@ -0,0 +1,164 @@
+package model;
+
+public abstract class Loja {
+	private int codigo;
+	private String logotipo;  
+	private String nome;
+	private String tipo;
+	private String endereco;
+	private String bairro;
+	private String cep;
+	private String estado;
+	private String cidade;
+	private String telefone;
+	private String cnpj;
+	private String responsavel;
+	private String email;
+	private String areasEntrega;
+	private String site;
+
+	public Loja(int codigo, String logotipo,String nome,String tipo,String endereco,String bairro,
+			String cep,String estado,String cidade,String telefone,String cnpj,String responsavel,String email,
+			String areasEntrega,String site) {
+		// TODO Auto-generated constructor stub
+		super();
+		this.codigo = codigo;
+		this.logotipo = logotipo;
+		this.nome = nome;
+		this.tipo = tipo;
+		this.endereco = endereco;
+		this.bairro = bairro;
+		this.cep = cep;
+		this.estado = estado;
+		this.cidade = cidade;
+		this.telefone = telefone;
+		this.cnpj = cnpj;
+		this.responsavel = responsavel;
+		this.email = email;
+		this.areasEntrega = areasEntrega;
+		this.site = site;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getLogotipo() {
+		return logotipo;
+	}
+
+	public void setLogotipo(String logotipo) {
+		this.logotipo = logotipo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereço) {
+		this.endereco = endereco;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getResponsavel() {
+		return responsavel;
+	}
+
+	public void setResponsavel(String responsavel) {
+		this.responsavel = responsavel;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAreasEntrega() {
+		return areasEntrega;
+	}
+
+	public void setAreasEntrega(String areasEntrega) {
+		this.areasEntrega = areasEntrega;
+	}
+
+	public String getSite() {
+		return site;
+	}
+
+	public void setSite(String site) {
+		this.site = site;
+	}
+	
+	
+
+}
