d1656a7e92f45e118cbd8e8ce78bb0de7d78d531
Camila <mylla.happy@gmail.com>
2014-01-21 22:48:01 -0300

    tela cadastro adm; remocao das factory

--- FramDeliveryOnline/src/model/FactoryProduto.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public interface FactoryProduto {
-
-	public abstract Produto create(String codigo, String imagem, String nome, String tipo,
-			String descricao, int qtdEstoque, int qtdVendida, double preco,
-			double desconto, double peso);
-	
-}
