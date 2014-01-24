6f8d74bf8adae7e07f3b807d1db90ecd6f080b3a
Mylla <mylla.happy@gmail.com>
2014-01-24 15:04:18 -0200

    Update ClienteBusiness.java

--- FramDeliveryOnline/src/controller/ClienteBusiness.java
+++ FramDeliveryOnline/src/controller/ClienteBusiness.java
@@ -12,10 +12,10 @@ public class ClienteBusiness {
 	
 	public boolean Cadastra(String nome, String cpf,String dtNasc, String email,String login, String senha,String endereco, String cidade,String estado, String cep) {
 
-		ConfInicial ci = new ConfInicial();
-		ci.configura();
+		BaseDados bd = new BaseDados();
+		bd.configura();
 		boolean cadastro = false;
-		List<Cliente> clientes = ci.getClientes();
+		List<Cliente> clientes = bd.getClientes();
 		
 		Cliente c = new Cliente("", nome, cpf, dtNasc, endereco, cidade, estado, cep, email, login, senha);
 		
