5748feba41d501b119d9eefba59d6e7033d51a35
Mylla <mylla.happy@gmail.com>
2014-01-24 15:06:21 -0200

    Update ProdutoBusiness.java

--- FramDeliveryOnline/src/controller/ProdutoBusiness.java
+++ FramDeliveryOnline/src/controller/ProdutoBusiness.java
@@ -12,8 +12,8 @@ public class ProdutoBusiness {
 	
 	public boolean cadastra(String observers,String codigo,String nomeLoja,String imagem,String nome,String tipo,String descricao,int qtdEstoque,double preco,double desconto,double peso) {
 
-		ConfInicial ci = new ConfInicial();
-		ci.configura();
+		BaseDados bd = new BaseDados();
+		bd.configura();
 		boolean cadastro = false;
 		//List<Produto> produtos = ci.getProdutos();
 		
