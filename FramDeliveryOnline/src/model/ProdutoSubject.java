953c1a5168a0fdf41f25b8965e16a331bb9e914f
amandaabelha <amandaabelha@hotmail.com>
2014-02-07 14:55:24 -0300

    recoloquei o metodo remove pelo indice em ProdutoSubject

--- FramDeliveryOnline/src/model/ProdutoSubject.java
+++ FramDeliveryOnline/src/model/ProdutoSubject.java
@@ -13,6 +13,8 @@ public interface ProdutoSubject { //p cd produto deve implementar essa interface
 	
 	public void remove(ClienteObserver observer);
 	
+	public void remove(int indice);
+	
 	public void notificarObservers();
 	
 }
