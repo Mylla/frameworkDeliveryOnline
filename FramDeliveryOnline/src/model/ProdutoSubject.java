acf20224d836fc06c6007dee1d5c4db9b0096bca
amandaabelha <amandaabelha@hotmail.com>
2014-02-07 14:52:46 -0300

    removi o metodo remove que recebe um inteiro como parametro de produtoSubject

--- FramDeliveryOnline/src/model/ProdutoSubject.java
+++ FramDeliveryOnline/src/model/ProdutoSubject.java
@@ -13,8 +13,6 @@ public interface ProdutoSubject { //p cd produto deve implementar essa interface
 	
 	public void remove(ClienteObserver observer);
 	
-	public void remove(int indice);
-	
 	public void notificarObservers();
 	
 }
